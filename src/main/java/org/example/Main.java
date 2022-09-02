955d1b842cfa350d11ad7edc47a49a193141b0b1
k-hyeongi <21900215@handong.ac.kr>
2022-09-02 10:29:59 +0900

    Test Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
         System.out.println("Hello world!");
+
+        System.out.print("이름 입력 : ");
+        String name = s.nextLine();
+
+        System.out.println("["+name+"]님 환영합니다~");
+
     }
+
 }
