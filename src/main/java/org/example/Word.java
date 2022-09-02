b21f633e446859cc4fea1a1bcb7d8f04a434c729
k-hyeongi <21900215@handong.ac.kr>
2022-09-02 10:24:09 +0900

    Initial commit

---
+++ src/main/java/org/example/Word.java
@@ -0,0 +1,23 @@
+package org.example;
+
+// data
+public class Word {
+    private String name;
+    private String meaning;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+}
